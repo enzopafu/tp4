1994074a01a428898dc2dc9e32f77e0ea94c64ae
enzopafu <enzopafu@gmail.com>
2020-10-03 18:46:18 -0300

    desarrollo del codigo

--- tp4.py
+++ tp4.py
@@ -1,10 +1,21 @@
-#Consigna: crear un programa el cual le permita al usuario:
-# 1. ingresar nombre y apellido.
-# 2. ingresar su n° de DNI.
-# 3. elegir a que partido politico desea votar:
-#   opcion A partido politico pro.
-#   opcion B partido politico K.
-#   opcion C partido politico de izq.
-#   opcion D otro partido politico.
-# 4. mostrar los datos ingresado y el voto realizado.
-# 5. en el caso de no haber elegido bien indicar " opcion mal ingresada".
+#!Consigna: crear un programa el cual le permita al usuario:
+#!1. ingresar nombre y apellido.
+#!2. ingresar su n° de DNI.
+#!3. elegir a que partido politico desea votar:
+#!  opcion A partido politico pro.
+#!  opcion B partido politico K.
+#!   opcion C partido politico de izq.
+#!  opcion D otro partido politico.
+#!4. mostrar los datos ingresado y el voto realizado.
+#!5. en el caso de no haber elegido bien indicar " opcion mal ingresada".
+
+N= input("ingrese nombre y apellido")
+D= input("ingrese numero de DNI")
+print("""
+Opciones a elegir:
+A. partido politico pro
+B. partido politico K
+C. partido politico de izquierda
+D. otro partido politico
+""")
+C= input("ingrese que opcion desea votar")
