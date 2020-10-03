39028c5203cfcf43878ee9f6cc3e2181e4835549
enzopafu <enzopafu@gmail.com>
2020-10-03 18:22:11 -0300

    ingreso de consignas

--- tp4.py
+++ tp4.py
@@ -1,1 +1,10 @@
-
+#Consigna: crear un programa el cual le permita al usuario:
+# 1. ingresar nombre y apellido.
+# 2. ingresar su n° de DNI.
+# 3. elegir a que partido politico desea votar:
+#   opcion A partido politico pro.
+#   opcion B partido politico K.
+#   opcion C partido politico de izq.
+#   opcion D otro partido politico.
+# 4. mostrar los datos ingresado y el voto realizado.
+# 5. en el caso de no haber elegido bien indicar " opcion mal ingresada".
